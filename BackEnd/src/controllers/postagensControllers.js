79734cea7ab2481020b606e62ea0fe8875570039
Maria Eduarda <eduardacampos652@gmail.com>
2024-09-09 08:00:49 -0300

    RF02 Concluida

--- BackEnd/src/controllers/postagensControllers.js
+++ BackEnd/src/controllers/postagensControllers.js
@@ -17,3 +17,31 @@ export const createPostagem = async (request, response) => {
         response.status(500).json({ message: "Erro ao publicar a postagem" })
     }
 }
+
+//listar postagens RF02
+export const getPostagems = async (request, response) => {
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 10;
+    const offset = (page - 1) * limit;
+    try {
+        const postagens = await Postagem.findAndCountAll({
+            limit,
+            offset,
+        });
+
+        const totalPaginas = Math.ceil(postagens.count / limit);
+        response.status(200).json({
+            totalPostagens: postagens.count,
+            totalPaginas,
+            paginaAtual: page,
+            itensPorPagina: limit,
+            proximaPagina:
+                totalPaginas === 0
+                    ? null
+                    : `http://localhost:3333/postagens?page=${page + 1}`,
+            postagens: postagens.rows
+        })
+    } catch (error) {
+        response.status(500).json({ message: "Erro ao buscar postagens" });
+    }
+}
